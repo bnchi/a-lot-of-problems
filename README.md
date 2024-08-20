6f981be5579599a55a3cfb223ca54a66e1204e89
Bnchi <bnchifahmi@gmail.com>
2024-08-20 08:51:23 +0300

    Refactor and add a readme

---
+++ README.md
@@ -0,0 +1,3 @@
+| ID | Solution |
+|----------|----------|
+| 1480 | [solution](/bnchi/a-lot-of-problems/blob/main/src/solutions/s_1480.rs) |
--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,57 @@ trait AppCommand {
     fn run(&self) -> anyhow::Result<()>;
 }
 
+struct RunProblem {
+    problem_id: u64,
+}
+
+impl AppCommand for RunProblem {
+    fn init(mut context: AppContext) -> anyhow::Result<Self> {
+        let problem_id = context.next_arg("problem id")?;
+        let problem_id = problem_id
+            .parse::<u64>()
+            .context("The problem id must be a valid u64")?;
+        Ok(RunProblem { problem_id })
+    }
+
+    fn run(&self) -> anyhow::Result<()> {
+        let status = Command::new("cargo")
+            .arg("test")
+            .arg(format!("test_{}", self.problem_id))
+            .status()?;
+
+        if !status.success() {
+            exit(1);
+        }
+
+        Ok(())
+    }
+}
+
+struct Template;
+
+impl Template {
+    fn get(ds: &DataStructure, problem_id: u64) -> anyhow::Result<String> {
+        let content = match ds {
+            DataStructure::Default => std::fs::read_to_string(Path::new("templates/default.txt"))?,
+        };
+        Ok(Self::replace_mustache(content, problem_id))
+    }
+
+    fn replace_mustache(content: String, problem_id: u64) -> String {
+        content.replace("{{problem_id}}", &problem_id.to_string())
+    }
+}
+
 enum DataStructure {
     Default,
 }
 
-impl DataStructure {
-    fn get_problem_ds(arg: anyhow::Result<String>) -> DataStructure {
-        if let Ok(ds_choosen) = arg {
-            match ds_choosen {
-                _ => DataStructure::Default,
-            }
-        } else {
-            DataStructure::Default
+impl From<String> for DataStructure {
+    fn from(value: String) -> Self {
+        match value {
+            _ => DataStructure::Default,
         }
-    }
-
-    fn get_source_with_ds(ds: &Self) -> anyhow::Result<String> {
-        let content = match ds {
-            DataStructure::Default => std::fs::read_to_string(Path::new("templates/default.txt"))?,
-        };
-
-        Ok(content)
     }
 }
 
@@ -44,15 +74,11 @@ struct Scaffold {
 
 impl AppCommand for Scaffold {
     fn init(mut context: AppContext) -> anyhow::Result<Self> {
-        let Some(problem_id) = context.args.next() else {
-            anyhow::bail!("The problem id must exist");
-        };
-
+        let problem_id = context.next_arg("problem id")?;
         let problem_id = problem_id
             .parse::<u64>()
             .context("The problem id must be a valid u64")?;
-
-        let ds = DataStructure::get_problem_ds(context.next_arg());
+        let ds = DataStructure::from(context.next_arg("data structure")?);
         Ok(Scaffold { problem_id, ds })
     }
 
@@ -60,9 +86,7 @@ impl AppCommand for Scaffold {
         let file_name = format!("src/solutions/s_{}.rs", self.problem_id);
         let path = Path::new(&file_name);
         let mut file = std::fs::File::create(path)?;
-
-        file.write_all(DataStructure::get_source_with_ds(&self.ds)?.as_bytes())?;
-
+        file.write_all(Template::get(&self.ds, self.problem_id)?.as_bytes())?;
         let mut solutions_mod = std::fs::File::options()
             .append(true)
             .open(Path::new("src/solutions.rs"))?;
@@ -83,25 +107,26 @@ impl AppContext {
         args.next();
 
         let Some(command) = args.next() else {
-            panic!("Must provide a command");
+            panic!("Must provide a valid command");
         };
 
         Self { args, command }
     }
 
-    pub fn next_arg(&mut self) -> anyhow::Result<String> {
-        self.args
-            .next()
-            .ok_or(anyhow::anyhow!("The argument wasn't passed"))
+    pub fn next_arg(&mut self, arg_name: &str) -> anyhow::Result<String> {
+        self.args.next().ok_or(anyhow::anyhow!(
+            "Must provide the {} as a an argument",
+            arg_name
+        ))
     }
 }
 
 fn start(context: AppContext) -> anyhow::Result<()> {
     match context.command.as_str() {
         //"latest" | "l" => run_latest_problem()?,
-        //"problem" => run_problem(&mut args)?,
+        "problem" => run_command::<RunProblem>(context)?,
         "scaffold" => run_command::<Scaffold>(context)?,
-        _ => anyhow::bail!("The command can't be recognized"),
+        _ => anyhow::bail!("The command {} can't be recognized", context.command),
     };
 
     Ok(())
@@ -113,56 +138,33 @@ fn run_command<T: AppCommand>(context: AppContext) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run_problem(args: &mut Args) -> anyhow::Result<()> {
-    let problem_id = args
-        .next()
-        .unwrap()
-        .parse::<u64>()
-        .context("The problem id must be a valid u64")?;
-
-    let status = Command::new("cargo")
-        .arg("test")
-        .arg(format!("test_{}", problem_id))
-        .status()?;
-
-    if !status.success() {
-        exit(1);
-    }
-
-    Ok(())
-}
-
-fn run_latest_problem() -> anyhow::Result<()> {
-    let file =
-        std::fs::File::open(Path::new("src/solutions.rs")).expect("The solutions file must exist");
-    let buf = RevBufReader::new(file);
-    let last_line: String = buf
-        .lines()
-        .take(1)
-        .map(|value| value.expect("to parse the line"))
-        .collect();
-
-    let (_, problem_id) = last_line.split_once("_").unwrap();
-    let problem_id = &problem_id[..problem_id.len() - 1];
-
-    let status = Command::new("cargo")
-        .arg("test")
-        .arg(format!("test_{}", problem_id))
-        .status()?;
-
-    if !status.success() {
-        exit(1);
-    }
-
-    Ok(())
-}
-
 fn main() -> anyhow::Result<()> {
     let context = AppContext::new(std::env::args());
     start(context)?;
     Ok(())
 }
 
-//fn assing_vars(content: &str, vars: Vars) -> String {
-//    content.replace("{{problem_id}}", &vars.problem_id.to_string())
+//fn run_latest_problem() -> anyhow::Result<()> {
+//    let file =
+//        std::fs::File::open(Path::new("src/solutions.rs")).expect("The solutions file must exist");
+//    let buf = RevBufReader::new(file);
+//    let last_line: String = buf
+//        .lines()
+//        .take(1)
+//        .map(|value| value.expect("to parse the line"))
+//        .collect();
+//
+//    let (_, problem_id) = last_line.split_once("_").unwrap();
+//    let problem_id = &problem_id[..problem_id.len() - 1];
+//
+//    let status = Command::new("cargo")
+//        .arg("test")
+//        .arg(format!("test_{}", problem_id))
+//        .status()?;
+//
+//    if !status.success() {
+//        exit(1);
+//    }
+//
+//    Ok(())
 //}
